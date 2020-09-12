d45b5bf382003c6ae3db9a493986347a9f1520ca
Phakarat Khongphaisan <59839532+pakarat044@users.noreply.github.com>
2020-09-12 12:55:10 +0700

    test

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,6 +9,9 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -16,3 +19,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
